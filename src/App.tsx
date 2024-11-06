a6c43605e1fa2ca2c2c6df9bcd2eccb45b5025ad
basselalsayed <alsayed.bassel@gmail.com>
2024-11-06 20:16:14 +0000

    Dark mode button

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,20 @@ import './App.css';
 import { Navbar } from './components/navbar/navbar.component';
 
 import { Grid } from './components/grid/grid.component';
+import { useDarkMode } from 'usehooks-ts';
+import { useEffect } from 'react';
 
 function App() {
+  const { isDarkMode } = useDarkMode();
+
+  useEffect(() => {
+    const rootClasses = window.document.documentElement.classList;
+
+    rootClasses.remove('dark', 'light');
+
+    rootClasses.add(isDarkMode ? 'dark' : 'light');
+  }, [isDarkMode]);
+
   return (
     <main className='grid grid-flow-row w-full grid-cols-[100%] grid-rows-[91svh_1fr] sm:grid-rows-[1fr_92svh] '>
       <Grid />
---
+++ src/components/navbar/components/dark-mode-button.component.tsx
@@ -0,0 +1,16 @@
+import { Sun, Moon } from 'lucide-react';
+import { useDarkMode } from 'usehooks-ts';
+
+import { Button } from '@/components/ui/button';
+
+export const DarkModeButton = () => {
+  const { toggle } = useDarkMode();
+
+  return (
+    <Button variant='outline' size='icon' onClick={toggle}>
+      <Sun className='h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0' />
+      <Moon className='absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100' />
+      <span className='sr-only'>Toggle dark mode</span>
+    </Button>
+  );
+};
