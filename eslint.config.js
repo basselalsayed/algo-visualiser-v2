42e893ad2895b5d61e198c36d3cf6c51f052cf8b
basselalsayed <alsayed.bassel@gmail.com>
2024-11-07 16:33:07 +0000

    eslint flat config and additional rules

--- eslint.config.js
+++ eslint.config.js
@@ -1,13 +1,23 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from 'typescript-eslint';
+import eslintPluginUnicorn from 'eslint-plugin-unicorn';
+import importPlugin from 'eslint-plugin-import';
+import sort from 'eslint-plugin-sort';
+import eslintConfigPrettier from 'eslint-config-prettier';
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
 
 export default tseslint.config(
   { ignores: ['dist'] },
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    extends: [
+      js.configs.recommended,
+      ...tseslint.configs.recommended,
+      importPlugin.flatConfigs.recommended,
+      sort.configs['flat/recommended'],
+    ],
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2020,
@@ -16,6 +26,8 @@ export default tseslint.config(
     plugins: {
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
+      unicorn: eslintPluginUnicorn,
+      // prettier: eslintPluginPrettierRecommended,
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
@@ -23,6 +35,45 @@ export default tseslint.config(
         'warn',
         { allowConstantExport: true },
       ],
+
+      '@typescript-eslint/consistent-type-imports': [
+        'error',
+        {
+          fixStyle: 'inline-type-imports',
+
+          prefer: 'type-imports',
+        },
+      ],
+      'import/order': [
+        'error',
+        {
+          pathGroups: [
+            {
+              pattern: '@/**',
+              group: 'parent',
+            },
+          ],
+          'newlines-between': 'always',
+          named: true,
+          alphabetize: {
+            order: 'asc',
+          },
+        },
+      ],
+      'sort/imports': 'off',
+      'sort/import-members': 'off',
+    },
+    settings: {
+      'import/parsers': {
+        '@typescript-eslint/parser': ['.ts', '.tsx'],
+      },
+      'import/resolver': {
+        typescript: {
+          alwaysTryTypes: true,
+        },
+      },
     },
   },
-)
+  eslintPluginPrettierRecommended,
+  eslintConfigPrettier
+);
