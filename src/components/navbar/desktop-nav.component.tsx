00a8b5913300ed8a36ae5e5b1e35faa1e83c9ac5
Bassel Al-Sayed <53341220+basselalsayed@users.noreply.github.com>
2024-12-20 15:11:22 +0300

    remove jsx constants from desktop nav

--- src/components/navbar/desktop-nav.component.tsx
+++ src/components/navbar/desktop-nav.component.tsx
@@ -23,33 +23,6 @@ export const DesktopNav: FC = () => {
   const { algoRunning } = useIsRunning();
   const { t } = useTranslation();
 
-  const algoSection = (
-    <NavigationMenuItem>
-      <NavigationMenuTrigger disabled={algoRunning}>
-        {t('navbar.algorithms')}
-      </NavigationMenuTrigger>
-
-      <NavigationMenuContent className='bg-transparent'>
-        <div className='flex w-[500px] content-center justify-center py-6 backdrop-blur-lg'>
-          <AlgoForm className='p-0' />
-        </div>
-      </NavigationMenuContent>
-    </NavigationMenuItem>
-  );
-
-  const settingsSection = (
-    <NavigationMenuItem>
-      <NavigationMenuTrigger disabled={algoRunning}>
-        {t('navbar.settings')}
-      </NavigationMenuTrigger>
-      <NavigationMenuContent>
-        <div className='grid w-[450px] grid-cols-1 gap-6 p-6 px-14 backdrop-blur-lg'>
-          <SettingsForm />
-        </div>
-      </NavigationMenuContent>
-    </NavigationMenuItem>
-  );
-
   return (
     <NavigationMenu className='grad-border row-start-1 grid w-full max-w-full grid-flow-col items-center justify-between justify-items-stretch border-b-2 bg-background px-4 py-3'>
       <h1 className='pr-1 text-left'>{t('navbar.appName')}</h1>
@@ -57,8 +30,30 @@ export const DesktopNav: FC = () => {
         <QuickSearchButton />
         <DarkModeSwitch />
         <WallSwitch />
-        {algoSection}
-        {settingsSection}
+
+        <NavigationMenuItem>
+          <NavigationMenuTrigger disabled={algoRunning}>
+            {t('navbar.algorithms')}
+          </NavigationMenuTrigger>
+
+          <NavigationMenuContent className='bg-transparent'>
+            <div className='flex w-[500px] content-center justify-center py-6'>
+              <AlgoForm className='p-0' />
+            </div>
+          </NavigationMenuContent>
+        </NavigationMenuItem>
+
+        <NavigationMenuItem>
+          <NavigationMenuTrigger disabled={algoRunning}>
+            {t('navbar.settings')}
+          </NavigationMenuTrigger>
+          <NavigationMenuContent>
+            <div className='w-[450px] p-6 px-14'>
+              <SettingsForm />
+            </div>
+          </NavigationMenuContent>
+        </NavigationMenuItem>
+
         <ResetButton />
         <MazeButton />
         <RunButton />
