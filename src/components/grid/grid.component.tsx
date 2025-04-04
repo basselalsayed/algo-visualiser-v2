d140a24a79b02f785aee3b9091b8e0e2d03ddc8d
Bassel Al-Sayed <53341220+basselalsayed@users.noreply.github.com>
2025-04-04 11:57:05 +0100

    update useEventHandler until usehooks update for 19

--- src/components/grid/grid.component.tsx
+++ src/components/grid/grid.component.tsx
@@ -1,10 +1,11 @@
 import { memo, useCallback, useEffect, useRef } from 'react';
 import { match } from 'ts-pattern';
-import { useBoolean, useEventListener } from 'usehooks-ts';
+import { useBoolean } from 'usehooks-ts';
 import { useShallow } from 'zustand/react/shallow';
 
 import {
   useDimensions,
+  useEventListener,
   useGrid,
   useResizeObserver,
   useSettings,
