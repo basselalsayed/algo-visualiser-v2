47db527293f8d8741427db6528c9b01d894d40bf
Bassel Al-Sayed <53341220+basselalsayed@users.noreply.github.com>
2025-04-13 10:33:34 +0100

    simplify id types

--- src/lib/constants/id.ts
+++ src/lib/constants/id.ts
@@ -21,17 +21,17 @@ export const HTML_IDS = {
   },
 } as const;
 
+interface IID {
+  [k: string]: string | IID;
+}
+
 type Selectorify<T> = {
   [K in keyof T]: T[K] extends string ? `#${T[K]}` : Selectorify<T[K]>;
 };
 
-const mapIdsToSelectors = <T extends Record<string, unknown>>(
-  obj: T
-): Selectorify<T> =>
+const mapIdsToSelectors = <T extends IID>(obj: T): Selectorify<T> =>
   mapValues(obj, (val) =>
-    isPlainObject(val)
-      ? mapIdsToSelectors(val as Record<string, unknown>)
-      : `#${val}`
+    isPlainObject(val) ? mapIdsToSelectors(val as IID) : `#${val as string}`
   ) as Selectorify<T>;
 
 export const HTML_SELECTORS = mapIdsToSelectors(HTML_IDS);
