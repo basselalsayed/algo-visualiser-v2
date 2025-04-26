2c9270cc4bc74b576afa171778883767e5f85071
Bassel Al-Sayed <53341220+basselalsayed@users.noreply.github.com>
2025-04-26 10:39:40 +0100

    add mobile landscape & mobile portrait media queries

--- src/hooks/ui/use-device-queries.hook.ts
+++ src/hooks/ui/use-device-queries.hook.ts
@@ -7,19 +7,38 @@ export const breakPoints = {
 };
 
 export const isMobileMq = `(max-width: ${breakPoints.tablet}rem)`;
+export const isMobileLandscapeMq = `only screen and (
+     (
+       (max-height: 62rem) and (max-width: 35rem)
+     ) or
+     (
+       (max-height: 35rem) and (max-width: 62rem)
+     )
+   ) and (orientation: landscape)`;
+
 export const isMobileOrTabletMq = `(max-width: ${breakPoints.laptop}rem)`;
 export const isTabletMq = `(min-width: ${breakPoints.tablet}rem) and (max-width: ${breakPoints.laptop}rem)`;
 export const isDesktopMq = `(min-width: ${breakPoints.laptop}rem)`;
 
-export const useDeviceQueries = () => {
+interface Return {
+  isDesktop: boolean;
+  isMobile: boolean;
+  isMobileLandscape: boolean;
+  isMobileOrTablet: boolean;
+  isTablet: boolean;
+}
+
+export const useDeviceQueries = (): Return => {
   const isMobile = useMediaQuery(isMobileMq);
   const isMobileOrTablet = useMediaQuery(isMobileOrTabletMq);
   const isTablet = useMediaQuery(isTabletMq);
   const isDesktop = useMediaQuery(isDesktopMq);
+  const isMobileLandscape = useMediaQuery(isMobileLandscapeMq);
 
   return {
     isDesktop,
     isMobile,
+    isMobileLandscape,
     isMobileOrTablet,
     isTablet,
   };
