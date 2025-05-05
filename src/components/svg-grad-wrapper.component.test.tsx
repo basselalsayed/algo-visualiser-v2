e88f4aa2e03a89c2a69d6a736c33aa7ee9102795
Bassel Al-Sayed <53341220+basselalsayed@users.noreply.github.com>
2025-05-05 02:31:48 +0100

    svg gradient tests

---
+++ src/components/svg-grad-wrapper.component.test.tsx
@@ -0,0 +1,75 @@
+import { render } from '@testing-library/react';
+import { type ComponentProps, type FC } from 'react';
+
+import { SVGGradWrapper } from './svg-grad-wrapper.component';
+
+const Component: FC<ComponentProps<'div'>> = (props) => {
+  return (
+    <div {...props}>
+      <svg>
+        <path />
+        <circle />
+      </svg>
+    </div>
+  );
+};
+const Component2: FC<ComponentProps<'svg'>> = (props) => {
+  return (
+    <svg {...props}>
+      <path />
+      <circle />
+    </svg>
+  );
+};
+
+describe('SVGGradWrapper', () => {
+  it('clones children and adds a gradient to nested svg and child elements', () => {
+    const { container } = render(
+      <SVGGradWrapper color1='red' color2='blue'>
+        <Component />
+      </SVGGradWrapper>
+    );
+
+    const lg = container.querySelector<SVGLinearGradientElement>(
+      'defs linearGradient'
+    );
+
+    expect(lg).toBeTruthy();
+
+    expect(lg?.style.getPropertyValue('--grad-1')).toBe('red');
+    expect(lg?.style.getPropertyValue('--grad-2')).toBe('blue');
+
+    expect(
+      container.querySelector('path')?.getAttribute('stroke')
+    ).toBeTruthy();
+
+    expect(
+      container.querySelector('circle')?.getAttribute('stroke')
+    ).toBeTruthy();
+  });
+
+  it('clones children and adds a gradient to direct svg and child elements', () => {
+    const { container } = render(
+      <SVGGradWrapper color1='red' color2='blue'>
+        <Component2 />
+      </SVGGradWrapper>
+    );
+
+    const lg = container.querySelector<SVGLinearGradientElement>(
+      'defs linearGradient'
+    );
+
+    expect(lg).toBeTruthy();
+
+    expect(lg?.style.getPropertyValue('--grad-1')).toBe('red');
+    expect(lg?.style.getPropertyValue('--grad-2')).toBe('blue');
+
+    expect(
+      container.querySelector('path')?.getAttribute('stroke')
+    ).toBeTruthy();
+
+    expect(
+      container.querySelector('circle')?.getAttribute('stroke')
+    ).toBeTruthy();
+  });
+});
